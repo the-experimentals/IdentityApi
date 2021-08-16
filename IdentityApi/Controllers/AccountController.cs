89bfcef80ca4e49a9e81a1bf9bd3baffb45baaac
Bibek Saini <itsbibeksaini@gmail.com>
2021-08-16 13:10:52 +0530

    Adding endpoint controllers

---
+++ IdentityApi/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace IdentityApi.Controllers
+{
+    [Route("account")]
+    public class AccountController : Controller
+    {
+        [HttpGet("test")]
+        public IActionResult Test()
+        {
+            return Ok("Testing account endpoint");
+        }
+    }
+}
