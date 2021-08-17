784537f03e182fa8b4f4c163addf95084b73ce54
Bibek Saini <itsbibeksaini@gmail.com>
2021-08-17 23:17:29 +0530

    Request and response models

---
+++ IdentityApi/ResponseModels/LogInResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IdentityApi.ResponseModels
+{
+    public class LogInResponse
+    {
+        public bool IS_AUTHENTICATED { get; set; }
+        public bool IS_VERIFIED { get; set; }
+        public List<string> ERRORS { get; set; } = new List<string>();
+        public string PROFILE_ID { get; set; }
+        public string NAME { get; set; }
+        public bool HAS_PWNED_PASSWORD { get; set; }
+
+        public TokenResponse TOKEN { get; set; } = new();
+    }
+}
---
+++ IdentityApi/ResponseModels/TokenResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace IdentityApi.ResponseModels
+{
+    public class TokenResponse
+    {
+        public string ACCESS { get; set; }
+        public string REFRESH { get; set; }
+        public bool ALLOW_REFRESH { get; set; }
+        public bool IS_REFRESHED { get; set; }
+        public int TTL { get; set; } = 5;
+    }
+}
