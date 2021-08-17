8f681ae0ad40326a208d6df081a8571b6db7780a
Bibek Saini <itsbibeksaini@gmail.com>
2021-08-17 23:19:47 +0530

    Adding auth manager unit test :: Authenticate

---
+++ IdentityApiTest/Auth/AuthManagerTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using IdentityApi.Auth;
+using IdentityApi.ResponseModels;
+using IdentityApiTest.Mockings;
+using IdentityApiTest.Ordering;
+using Moq;
+using Xunit;
+
+namespace IdentityApiTest.Auth
+{
+    [TestCaseOrderer("IdentityApiTest.Ordering.PriorityOrder", "IdentityApiTest")]
+    public class AuthManagerTest : IClassFixture<IdentityStoreMock>, IClassFixture<TMCacheMock>
+    {
+        private readonly IAuthManager _authManager;
+        public AuthManagerTest(IdentityStoreMock storeMock, TMCacheMock cacheMock)
+        {            
+            _authManager = new AuthManager(storeMock._store, cacheMock._cache);
+        }
+
+        [Fact(DisplayName = "Test authenictate user"), Priority(1)]
+        public void TestAuthenticate()
+        {
+            LogInResponse result = _authManager.Authenticate(new()
+            {
+                USERNAME = "testuser",
+                PASSWORD = "testPassword"
+            });
+
+            Assert.True(result.IS_AUTHENTICATED);
+        }
+    }
+}
---
+++ IdentityApiTest/Mockings/TMCacheMock.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using IdentityApi.Account;
+using IdentityApi.DataModels;
+using IdentityApi.Utilities;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace IdentityApiTest.Mockings
+{
+    public class TMCacheMock : IDisposable
+    {
+        public TMCache _cache { get; private set; }
+        public string testProfileGuid = Guid.NewGuid().ToString();
+
+        public TMCacheMock()
+        {
+            _cache = new(new MemoryCache(new MemoryCacheOptions()));
+
+            _cache.Add<string>("testuser", testProfileGuid);
+
+            UserSecret userSecret = Utility.GetUserSecret(null, "testPassword");
+
+            Credential credential = new()
+            {
+                ID = Guid.NewGuid().ToString(),
+                USERNAME = "testuser",
+                PROFILE_ID = testProfileGuid,
+                SALT = userSecret.SALT,
+                SECRET_HASH = userSecret.SECRET_HASH
+            };
+
+            Profile profile = new()
+            {
+                ID = testProfileGuid,
+                NAME = "default test user",
+                NEW = true,
+                CREATED_BY = "system",
+                EMAIL = "test@test.com",
+                CREDENTIAL = credential
+            };
+
+            _cache.Add<Profile>(Profile.PROFILE_CACHE_KEY + profile.ID, profile);
+        }
+
+        public void Dispose()
+        {
+            _cache.Dispose();
+        }
+    }
+}
