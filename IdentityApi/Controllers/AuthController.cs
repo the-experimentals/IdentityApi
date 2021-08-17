f7cfa3ee433eab42a6179847d7b1e5b8c5ac3e3c
Bibek Saini <itsbibeksaini@gmail.com>
2021-08-17 23:19:27 +0530

    Adding LogIn endpoint

--- IdentityApi/Controllers/AuthController.cs
+++ IdentityApi/Controllers/AuthController.cs
@@ -1,20 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Threading.Tasks;
+using IdentityApi.Auth;
+using IdentityApi.Mappings;
+using IdentityApi.RequestModels;
+using IdentityApi.ResponseModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityApi.Controllers
 {
-    [Route("auth")]
+    [Authorize]
+    [ApiController]
+    [Route(AuthMappings.ENDPOINT_ROUTE)]
+    [Produces(MediaTypeNames.Application.Json)]
     public class AuthController : Controller
     {
+        private readonly IAuthManager _authManager;
+
+        public AuthController(IAuthManager authManager)
+        {
+            _authManager = authManager;
+        }
+
         [AllowAnonymous]
         [HttpGet("test")]
         public IActionResult Test()
         {
             return Ok("Testing auth endpoint");
+        }
+
+        /// <summary>
+        /// Endpoint action for accepting client's request to validate account credentials.
+        /// </summary>
+        /// <param name="logInRequest">Request model for containing user's IDENTIFIER and SECRET.</param>
+        /// <returns>Login response which either contains authentication success status or errors for faliure.</returns>
+        /// <response code="200">User's credentials have been authenticated successfully</response>
+        /// <response code="401">Unsuccessfull user's authentication, check errors in response model.</response> 
+        [AllowAnonymous]
+        [HttpPost(AuthMappings.LOG_IN)]
+        [ProducesResponseType(typeof(LogInResponse), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(List<string>), StatusCodes.Status401Unauthorized)]
+        public IActionResult LogIn([FromBody]LogInRequest logInRequest)
+        {
+            return Unauthorized();
         }
     }
 }
