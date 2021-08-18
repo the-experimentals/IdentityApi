63c745c20a9ff438e7cce305ba80d22405e9cece
Bibek Saini <itsbibeksaini@gmail.com>
2021-08-18 23:52:02 +0530

    Adding auto mapper mapping profiles

---
+++ IdentityApi/Mappings/MappingProfiles.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using IdentityApi.ResponseModels;
+
+namespace IdentityApi.Mappings
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {            
+            CreateMap<PolicyApi.Protos.TokenResponse, TokenResponse>()
+                .ForMember(dest => dest.ACCESS, opt => opt.MapFrom(src => src.ACCESS));
+
+            //CreateMap<Toggle2fa, Protos.toggle2faRequest>()
+            //    .ForMember(dest => dest.ENABLED, opt => opt.MapFrom(src => src.ENABLED));
+        }
+    }
+}
